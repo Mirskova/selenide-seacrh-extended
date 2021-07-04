4e07eb4e9203cb6aa9fb561dfbdbb3548220603f
Olga Mirskova <mirskova@yandex.ru>
2021-07-04 17:51:32 +0300

    Changed files

--- src/test/java/guru/qa/tests/TextBoxTests.java
+++ src/test/java/guru/qa/tests/TextBoxTests.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -22,7 +23,13 @@ public class TextBoxTests {
         $("#userEmail").setValue("anna@mail.org");
         $("#currentAddress").setValue("city street house");
         $("#permanentAddress").setValue("city street house apt");
+        $("#submit").scrollIntoView(true);
         $("#submit").click();
 
+        $("#name").shouldHave(text("Anna"));
+        $("#email").shouldHave(text("anna@mail.org"));
+        $("#currentAddress").shouldHave(text("city street house"));
+        $("#permanentAddress").shouldHave(text("city street house apt"));
+        $("#submit").click();
     }
 }
