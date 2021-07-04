11fb781c4ea9c7521be14b84f84f80a68f8c7b44
Olga Mirskova <mirskova@yandex.ru>
2021-07-04 21:49:50 +0300

    Added CSS/XPATH examples

--- src/test/java/guru/qa/tests/TextBoxTests.java
+++ src/test/java/guru/qa/tests/TextBoxTests.java
@@ -12,24 +12,24 @@ public class TextBoxTests {
 
     @BeforeAll
     static void setup() {
-        Configuration.baseUrl = "https://demoqa.com/";
+        Configuration.baseUrl = "https://demoqa.com";
         Configuration.startMaximized = true;
     }
 
     @Test
-     void positiveFillTest() {
+    void positiveFillTest() {
         open("/text-box");
         $("#userName").setValue("Anna");
         $("#userEmail").setValue("anna@mail.org");
         $("#currentAddress").setValue("city street house");
         $("#permanentAddress").setValue("city street house apt");
-        $("#submit").scrollIntoView(true);
         $("#submit").click();
 
-        $("#name").shouldHave(text("Anna"));
-        $("#email").shouldHave(text("anna@mail.org"));
-        $("#currentAddress").shouldHave(text("city street house"));
-        $("#permanentAddress").shouldHave(text("city street house apt"));
-        $("#submit").click();
+        $("p#name").shouldHave(text("Anna"));
+        $("p#email").shouldHave(text("anna@mail.org"));
+        $("p#currentAddress").shouldHave(text("city street house"));
+        $("p#permanentAddress").shouldHave(text("city street house apt"));
+
     }
+
 }
